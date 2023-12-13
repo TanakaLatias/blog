21340714320318e636533c87c12e728fc29adabb
TanakaLatias <109828366+TanakaLatias@users.noreply.github.com>
2023-12-13 16:01:12 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 import os
-STATIC_ROOT = '//'
+#STATIC_ROOT = '//'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
+#STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
